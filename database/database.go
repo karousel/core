2686d09fd8b18b394880af5b78529e199dd4c775
Mihir Singh <git.service@mihirsingh.com>
2015-03-29 15:00:40 -0500

    Added creation of the collections table to the database package

--- database/database.go
+++ database/database.go
@@ -3,6 +3,8 @@ package database
 import (
 	"database/sql"
 
+	"github.com/karousel/core/collection"
+
 	_ "github.com/lib/pq"
 	gorp "gopkg.in/gorp.v1"
 )
@@ -17,6 +19,17 @@ func NewDatabase(datasource string) (*gorp.DbMap, error) {
 	}
 
 	databaseMap = &gorp.DbMap{Db: database, Dialect: gorp.PostgresDialect{}}
+
+	collectionsTable := databaseMap.AddTableWithName(collection.Collection{}, "collections")
+	collectionsTable.SetKeys(true, "id")
+	collectionsTable.ColMap("name").SetNotNull(true)
+	collectionsTable.ColMap("name").SetUnique(true)
+
+	err = databaseMap.CreateTablesIfNotExists()
+
+	if err != nil {
+		return databaseMap, err
+	}
 
 	return databaseMap, nil
 }
