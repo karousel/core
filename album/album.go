02202d679e7a9ab11e24a02d1e39a3ffac42f5fc
Mihir Singh <git.service@mihirsingh.com>
2015-03-29 15:26:31 -0500

    Defined the database tags for the album struct

--- album/album.go
+++ album/album.go
@@ -1,7 +1,7 @@
 package album
 
 type Album struct {
-	Id           int64
-	CollectionId int64
-	Name         string
+	Id           int64  `db:"id"`
+	CollectionId int64  `db:"collection_id"`
+	Name         string `db:"name"`
 }
