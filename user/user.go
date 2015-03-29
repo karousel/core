5f2b2062f611f67cf0df785588cbcccca4fb2298
Mihir Singh <git.service@mihirsingh.com>
2015-03-29 17:33:25 -0500

    Implemented a function which creates and returns a user

--- user/user.go
+++ user/user.go
@@ -1,9 +1,31 @@
 package user
 
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
 type User struct {
 	Id            int64  `db:"id"`
 	Name          string `db:"name"`
 	EmailAddress  string `db:"email_address"`
 	Password      string `db:"password"`
 	Administrator bool   `db:"is_administrator"`
+}
+
+func NewUser(name string, emailAddress string, password string, administrator bool) (User, error) {
+	var user User
+
+	user.Name = name
+	user.EmailAddress = emailAddress
+	user.Administrator = administrator
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+
+	if err != nil {
+		return user, err
+	}
+
+	user.Password = string(hash)
+
+	return user, nil
 }
