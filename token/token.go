d1996d6246ec09c061581a09f479eb7be20abd03
Mihir Singh <git.service@mihirsingh.com>
2015-03-29 20:32:38 -0500

    Added a function to create a token for user to the token package

--- token/token.go
+++ token/token.go
@@ -1,6 +1,7 @@
 package token
 
 import (
+	"crypto/rand"
 	"time"
 )
 
@@ -10,4 +11,23 @@ type Token struct {
 	Expires    bool      `db:"expires"`
 	Token      string    `db:"token"`
 	Expiration time.Time `db:"expiration"`
+}
+
+func NewTokenForUserId(user int64) (Token, error) {
+	var token Token
+	size := 64
+
+	value := make([]byte, size)
+	_, err := rand.Read(value)
+
+	if err != nil {
+		return token, err
+	}
+
+	token.UserId = user
+	token.Token = string(value)
+	token.Expires = true
+	token.Expiration = time.Now().UTC().Add(time.Hour * 72)
+
+	return token, nil
 }
